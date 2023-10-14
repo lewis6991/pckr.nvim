9dfeb783ff050a5059f00c89a1fea68e229dd9df
Zhou-Yicheng <zhou_yicheng@foxmail.com>
2023-10-14 23:58:24 +0800

    feature: add support for bind custom function to keys

--- lua/pckr/loader/keys.lua
+++ lua/pckr/loader/keys.lua
@@ -1,21 +1,31 @@
 --- @param mode string
 --- @param key string
+--- @param _rhs? string|fun()
+--- @param _opts? table
 --- @return fun(_: fun())
-return function(mode, key)
+return function(mode, key, _rhs, _opts)
+  if not _opts then
+    _opts = {
+      desc = 'pckr.nvim lazy load',
+      silent = true,
+    }
+  end
   --- @param loader fun()
   return function(loader)
     -- TODO(lewis6991): detect is mapping already exists
-    vim.keymap.set(mode, key, function()
-      vim.keymap.del(mode, key)
-      loader()
-      if mode == 'n' then
-        vim.api.nvim_input(key)
-      else
-        vim.api.nvim_feedkeys(key, mode, false)
-      end
-    end, {
-      desc = 'pckr.nvim lazy load',
-      silent = true,
-    })
+    -- TODO(Zhou-Yicheng): delete mapping if exists
+    -- vim.keymap.del(mode, key)
+    loader()
+    if _rhs then
+      vim.keymap.set(mode, key, _rhs, _opts)
+    else
+      vim.keymap.set(mode, key, function()
+        if mode == 'n' then
+          vim.api.nvim_input(key)
+        else
+          vim.api.nvim_feedkeys(key, mode, false)
+        end
+      end, _opts)
+    end
   end
 end
