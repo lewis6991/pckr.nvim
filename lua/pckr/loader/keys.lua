48efc3fe46ffbbd3d98e23a06c5978012fef5fac
Lewis Russell <lewis6991@gmail.com>
2024-01-26 10:39:21 +0000

    refactor: loader/keys.lua

--- lua/pckr/loader/keys.lua
+++ lua/pckr/loader/keys.lua
@@ -1,31 +1,33 @@
 --- @param mode string
 --- @param key string
---- @param _rhs? string|fun()
---- @param _opts? table
+--- @param rhs? string|fun()
+--- @param opts? vim.api.keyset.keymap
 --- @return fun(_: fun())
-return function(mode, key, _rhs, _opts)
-  if not _opts then
-    _opts = {
-      desc = 'pckr.nvim lazy load',
-      silent = true,
-    }
+return function(mode, key, rhs, opts)
+  opts = opts or {}
+
+  if opts.desc == nil then
+    opts.desc = 'pckr.nvim lazy load'
   end
+
+  if opts.silent == nil then
+    opts.silent = true
+  end
+
+  rhs = rhs or function()
+    if mode == 'n' then
+      vim.api.nvim_input(key)
+    else
+      vim.api.nvim_feedkeys(key, mode, false)
+    end
+  end
+
   --- @param loader fun()
   return function(loader)
     -- TODO(lewis6991): detect is mapping already exists
     -- TODO(Zhou-Yicheng): delete mapping if exists
     -- vim.keymap.del(mode, key)
     loader()
-    if _rhs then
-      vim.keymap.set(mode, key, _rhs, _opts)
-    else
-      vim.keymap.set(mode, key, function()
-        if mode == 'n' then
-          vim.api.nvim_input(key)
-        else
-          vim.api.nvim_feedkeys(key, mode, false)
-        end
-      end, _opts)
-    end
+    vim.keymap.set(mode, key, rhs, opts)
   end
 end
