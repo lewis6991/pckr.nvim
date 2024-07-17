b02d02cd951e98326b47d15eb65bf8ad74bd6b42
Epheien <epheien@163.com>
2024-07-17 23:45:05 +0800

    feat: simply re-issue the event for event cond load plugins (#29)
    Co-authored-by: eph <eph@MacBook-Pro.local>

--- lua/pckr/loader/event.lua
+++ lua/pckr/loader/event.lua
@@ -3,14 +3,24 @@
 --- @return fun(_: fun())
 return function(events, pattern)
   return function(loader)
+    local done = false
     vim.api.nvim_create_autocmd(events, {
       pattern = pattern,
       once = true,
       desc = 'pckr.nvim lazy load',
-      callback = function()
+      callback = function(ev)
+        if done then
+          return true
+        end
+        -- HACK: work-around for https://github.com/neovim/neovim/issues/25526
+        done = true
         loader()
-        -- TODO(lewis6991): should we re-issue the event? (#1163)
-        -- vim.api.nvim_exec_autocmds(event, { modeline = false })
+        vim.api.nvim_exec_autocmds(ev.event, {
+          buffer = ev.buf,
+          group = ev.group,
+          modeline = false,
+          data = ev.data,
+        })
       end,
     })
   end
