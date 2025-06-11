dcc0e2766d7a3a1911287fef7060ac07908cf376
Lewis Russell <lewis6991@gmail.com>
2025-06-11 10:08:38 +0100

    fix(event): do no reissue autocmd on lazy load
    Other consumers will be listening and will refire their callbacks.

--- lua/pckr/loader/event.lua
+++ lua/pckr/loader/event.lua
@@ -3,24 +3,12 @@
 --- @return fun(_: fun())
 return function(events, pattern)
   return function(loader)
-    local done = false
     vim.api.nvim_create_autocmd(events, {
       pattern = pattern,
       once = true,
       desc = 'pckr.nvim lazy load',
-      callback = function(ev)
-        if done then
-          return true
-        end
-        -- HACK: work-around for https://github.com/neovim/neovim/issues/25526
-        done = true
+      callback = function()
         loader()
-        vim.api.nvim_exec_autocmds(ev.event, {
-          buffer = ev.buf,
-          group = ev.group,
-          modeline = false,
-          data = ev.data,
-        })
       end,
     })
   end
